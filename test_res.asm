fb42fc6e709fc5f13306364a91830abacdddbc66
Irina Prygunova <prygunova.irina07@mail.ru>
2026-03-12 02:39:50 +0300

    обновление всех регистров

--- test_res.asm
+++ test_res.asm
@@ -8,19 +8,28 @@ locals @@
 
 Start:
 
-    @@loop:
-        in al, 60h
-        cmp al, 01h         ; esc         
-        je  @@done
-
         mov ax, 1111h
         mov bx, 2222h
         mov cx, 3333h
         mov dx, 4444h
         mov si, 5555h
         mov di, 6666h
+        mov bp, 7777h
+        push 8888h
+        pop ds
+        push 9999h
+        pop es
+        push 0000h
+        pop ss
 
-        jmp @@loop
+    @@loop:
+        cli
+        in al, 60h
+        cmp al, 01h         ; esc 
+        add sp, 10h  
+        mov al, 11h 
+        jne @@loop
+
 
     @@done:
         mov ax, 4c00h
